c6840d9f47820f8d591789e67f2fd2f8e26cd843
Sapphire930 <abhirajpatwa930@gmail.com>
2022-01-26 17:58:34 +0530

    First base stable version

---
+++ public/javascripts/sketch.js
@@ -0,0 +1,39 @@
+var socket;
+let colour = 'black';
+function setup() {
+  var canvas = createCanvas(800, 475);
+  canvas.parent("canvas");
+  size = 40;
+  socket = io.connect('http://localhost:3000');
+  socket.on('mouse',(data) => {
+    fill(data.c);
+    ellipse(data.x, data.y, data.s, data.s);
+  })
+  
+}
+
+function mouseDragged() {
+  //print(mouseX +','+mouseY);
+  var data= {
+    x: int(mouseX),
+    y: mouseY,
+    c: colour,
+    s: size
+  }
+  socket.emit('mouse', data);
+}
+
+const mkColour = (c) => colour = c;
+const mkSize = (s) => size = s;
+
+function draw() {
+  noStroke();
+  if (mouseIsPressed && mouseButton == 'left') {
+    fill(colour);
+    ellipse(int(mouseX), mouseY, size, size);
+  }
+  else if (mouseIsPressed && mouseButton == 'center') {
+    fill(255);
+    ellipse(mouseX, mouseY, size, size);
+  }
+}
---
+++ socketapi.js
@@ -0,0 +1,17 @@
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    console.log(`New connection - ${socket.id}`);
+
+    socket.on('mouse', (data) => {
+        socket.broadcast.emit('mouse', data);
+        console.log(data);
+    });
+});
+// end of socket.io logic
+
+module.exports = socketapi;
